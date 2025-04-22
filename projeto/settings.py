c23076893d9bf6427eb85e73a8098cd162c78523
FeliPerrelliACT <felipe.perrelli@engact.com.br>
2025-04-22 16:12:52 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -41,8 +41,8 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],  # Garante que a pasta templates está sendo considerada
-        'APP_DIRS': True,  # Habilita busca automática de templates dentro dos apps
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
